705a35c00d24017f757c972cc194411b0359752e
bokyeong <wntjdals0412@gmail.com>
2025-01-25 01:01:20 +0900

    [JOL]1523

---
+++ 보경/0124/Jol_1523_별삼각형1.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Jol_1523_별삼각형1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        if (m > 3 || m < 1 || n > 100) {
+            System.out.println("INPUT ERROR!");
+            return;
+        }
+
+        if (m == 1) {
+            for (int i = 1; i <= n; i++) {
+                System.out.println("*".repeat(i) );
+            }
+        }
+
+        if (m == 2) {
+            for(int i = n; i > 0; i--) {
+                System.out.println("*".repeat(i));
+            }
+        }
+
+        if (m ==3) {
+            int blank =  n-1;
+            for(int i = 1; i <= n; i++) {
+                System.out.println(" ".repeat(blank) + "*".repeat(i*2-1));
+                blank -= 1;
+            }
+        }
+
+        sc.close();
+    }
+}
+
+
